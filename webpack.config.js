3c1aa2c6cec6532f4c13ab439fc48ff2f7994bdc
jinchung <jin.chung17@gmail.com>
2018-07-27 14:52:07 -0400

    adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+    ]
+  },
+}

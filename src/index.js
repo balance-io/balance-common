c5309e958d5a7cee73af1516ab78ebe44a9cfd9e
jinchung <jin.chung17@gmail.com>
2018-07-26 20:15:12 -0400

    exposing more account methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { account, accountUpdateAccountAddress, send } from './reducers';
+import { account, accountUpdateAccountAddress, accountUpdateHasPendingTransaction, send } from './reducers';
 import * as commonStorage from './handlers/commonStorage';
 import * as bignumber from './helpers/bignumber';
 export { commonStorage, bignumber, account, accountUpdateAccountAddress };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import account from './_account';
 import send from './_send';
-import { accountUpdateAccountAddress } from './_account';
+import { accountUpdateAccountAddress, accountUpdateHasPendingTransaction } from './_account';
 
 export { account, accountUpdateAccountAddress, send };

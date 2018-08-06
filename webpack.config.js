ee013fc414ebe1deae2d4de6e6ec07786989c85d
jinchung <jin.chung17@gmail.com>
2018-08-06 11:54:17 -0400

    updating webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,22 @@ module.exports = {
     libraryTarget: 'commonjs2'
   },
   module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        include: [
+          path.join(__dirname, 'node_modules/react-native-storage')
+        ],
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          presets: ['es2015', 'stage-1', 'react'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ],
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
     ]
-  },
+  }
 }

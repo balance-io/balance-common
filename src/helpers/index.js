b82e29aaad1495fefdd3cd0cfeaffd9bf21736a6
jinchung <jin.chung17@gmail.com>
2019-01-14 17:10:12 -0500

    sortlist

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,3 @@
-import sortList from './sortList';
+import { default as sortList } from './sortList';
 import { getCountdown, getLocalTimeDate } from './time';
 export { getCountdown, getLocalTimeDate, sortList };

faa98381f689593db160f606b1be853dc9496269
jinchung <jin.chung17@gmail.com>
2019-02-04 19:48:19 -0500

    removing tmp comment

--- src/reducers/_transactions.js
+++ src/reducers/_transactions.js
@@ -58,7 +58,6 @@ export const transactionsRefreshState = () => (dispatch, getState) => {
       page: 1
     }));
   };
-  /*
   const { fetchingTransactions } = getState().transactions;
   console.log('fetching txns status', fetchingTransactions);
   if (!fetchingTransactions) {
@@ -66,7 +65,6 @@ export const transactionsRefreshState = () => (dispatch, getState) => {
   }
   clearInterval(getTransactionsInterval);
   getTransactionsInterval = setInterval(getTransactions, 15000); // 15 secs
-  */
 };
 
 export const transactionsAddNewTransaction = txDetails => (dispatch, getState) => new Promise((resolve, reject) => {

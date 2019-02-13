1941fb476114ef98210c7cf6b2c8b9769c1bfd33
jinchung <jin.chung17@gmail.com>
2019-02-13 17:45:06 -0500

    more tests for parsing txns

--- src/handlers/__tests__/parsers.test.js
+++ src/handlers/__tests__/parsers.test.js
@@ -1,6 +1,12 @@
 import '@babel/polyfill';
-import { mockPriceResponse } from './parsersMockData';
-import { parsePricesObject } from '../parsers'
+import {
+  mockERC20,
+  mockNonTokenTransfer,
+  mockNonTokenTransferKDO,
+  mockPriceResponse,
+  mockTransaction
+} from './parsersMockData';
+import { parsePricesObject, parseTransaction } from '../parsers'
 import nativeCurrencies from '../../references/native-currencies.json';
 
 test('parsePricesObject', () => {
@@ -17,4 +23,32 @@ test('parsePricesObject', () => {
       )
     )
   );
+});
+
+test('parseTransactionWhenTransfersReversed', () => {
+	const address = "0x203526aff6de28820e5eac5d5137f08d0395f3ca";
+  const result = parseTransaction([], mockTransaction, address);
+  expect(result).toHaveLength(2);
+  expect(result[0].to).toBe(address);
+  expect(result[1].from).toBe(address);
+});
+
+test('parseTransactionWhenNonTokenTransfer', () => {
+	const address = "0x203526aff6de28820e5eac5d5137f08d0395f3ca";
+  const result = parseTransaction([], mockNonTokenTransfer, address);
+  expect(result).toHaveLength(1);
+  expect(result[0].value.amount).toBe('1');
+});
+
+test('parseTransactionWhenNonTokenTransferWithValue', () => {
+	const address = "0x1492004547FF0eFd778CC2c14E794B26B4701105";
+  const result = parseTransaction([], mockNonTokenTransferKDO, address);
+  expect(result).toHaveLength(1);
+  expect(result[0].value.amount).toBe('0');
+});
+
+test('parseTransactionWithMultipleTransfers', () => {
+	const address = "0x1492004547FF0eFd778CC2c14E794B26B4701105";
+  const result = parseTransaction([], mockERC20, address);
+  expect(result).toHaveLength(1);
 });
